a5bae244f601a1c72a9d53c5ac39c0f9b8a7d519
MaulanaArum <111202012552@mhs.dinus.ac.id>
2022-01-16 22:48:17 +0700

    TA UAS PBW 2022

---
+++ TA_UAS_PBW_12552/main.js
@@ -0,0 +1,6 @@
+document.getElementById('log-in').onclick = function () {
+  window.location = 'login.html';
+};
+document.getElementById('contact-btn').onclick = function () {
+  window.location = 'https://www.instagram.com/maulanaarum__12/';
+};
